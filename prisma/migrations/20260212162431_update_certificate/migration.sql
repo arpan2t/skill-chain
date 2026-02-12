f5acdb55227eaa74c7c421cfd786ff3b0f5fd604
Arpan Bhattarai <bhattaraiarpan18@gmail.com>
2026-02-12 22:10:45 +0545

    chore: configure prisma@5

---
+++ prisma/migrations/20260212162431_update_certificate/migration.sql
@@ -0,0 +1,34 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "user_wallet" TEXT,
+    "user_type" INTEGER NOT NULL DEFAULT 0,
+    "image" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Certificate" (
+    "id" TEXT NOT NULL,
+    "destination_wallet" TEXT NOT NULL,
+    "nftAddress" TEXT NOT NULL,
+    "ipfsUrl" TEXT NOT NULL,
+    "mintedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "issuedById" TEXT NOT NULL,
+
+    CONSTRAINT "Certificate_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Certificate_nftAddress_key" ON "Certificate"("nftAddress");
+
+-- AddForeignKey
+ALTER TABLE "Certificate" ADD CONSTRAINT "Certificate_issuedById_fkey" FOREIGN KEY ("issuedById") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
